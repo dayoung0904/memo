475bcf93073100f88e7c35adf54fa7c6782ac472
학원 <test@test.com>
2023-10-27 21:43:22 +0900

    memo layout + 아이디 중복확인

---
+++ src/main/java/com/memo/user/bo/UserBO.java
@@ -0,0 +1,14 @@
+package com.memo.user.bo;
+
+import org.springframework.stereotype.Service;
+
+import com.memo.user.Entity.UserEntity;
+
+@Service
+public class UserBO {
+
+	// input:loginId		output:UserEntity(null or entity)
+	public UserEntity getUserEntityByLoginId(String loginId) {
+		return null;
+	}
+}
