40f8d731289af630d9ed55d18fd15b1063d3c5f2
LAPTOP-MUGJK49K\임다영 <gkfka8520@naver.com>
2023-10-31 17:12:19 +0900

    메모 유저  validation

--- src/main/java/com/memo/user/UserContrller.java
+++ src/main/java/com/memo/user/UserContrller.java
@@ -14,4 +14,10 @@ public class UserContrller {
 		model.addAttribute("viewName", "user/signUp");
 		return "template/layout";
 	}
+
+	@GetMapping("/sign-in-view")
+	public String signInView(Model model) {
+		model.addAttribute("viewName", "user/signIn");
+		return "template/layout";
+	}
 }
