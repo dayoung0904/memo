2428b589d8c84b98e7d6a2459d009f9c8df2e114
학원 <test@test.com>
2023-11-01 21:43:43 +0900

    POST CREATE

--- src/main/java/com/memo/post/bo/PostBO.java
+++ src/main/java/com/memo/post/bo/PostBO.java
@@ -18,4 +18,13 @@ public class PostBO {
 	public List<Post> getPostListByUserId(int userId){
 		return postMapper.selectPostListByUserId(userId);
 	}
+	
+	// input:파라미터들 		output:X
+	public void addPost(int userId, String subject, String content) {
+		String imagePath = null;
+		
+		// TODO : 이미지가 있으면 업로드
+		
+		postMapper.insertPost(userId, subject, content, imagePath);
+	}
 }
